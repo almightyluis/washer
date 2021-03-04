ae987f2ff9adbd23f0a7d9dca7e00bb5bf1134fc
Luis Gonzalez <otfgonzalez@gmail.com>
2021-03-03 19:26:34 -0800

    Replaced html home page with php

--- home.php
+++ home.php
@@ -1,3 +1,11 @@
+<?php
+
+date_default_timezone_set("America/Los_Angeles");
+$current_date = date("m/d/o");
+
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -33,6 +41,7 @@
               </ul>
             </div>
         </nav>
+
 
         <div id="mainImageContainer" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
@@ -190,20 +199,22 @@
                 </button>
               </div>
               <div class="modal-body">
-                <form action="bookAppointment.php" method="POST">
+                <form action="makeApp.html?name=exampleFullname" method="POST">
                   <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="exampleFullname">Full Name</label>
+                    <input type="text" class="form-control" id="exampleFullname" placeholder="First Last">
                   </div>
-                  <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+                  <div class="form-group">
+                    <label for="examplePhone">Phone Number</label>
+                    <input type="tel" class="form-control" id="examplePhone" placeholder="(XXX) XXX XXXX">
                   </div>
+                  
                   <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
               </div>
